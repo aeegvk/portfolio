9336db444211fbccafe55b0f3ea292021de434d9
aeegvk <aeegvk@gmail.com>
2024-06-17 09:11:56 +0800

    adds formatting and custom text to qr code

--- src/routes/qr/+page.svelte
+++ src/routes/qr/+page.svelte
@@ -6,9 +6,11 @@
 <section>
     <h3>Enter a link to generate a QR Code</h3>
     <input type="text" placeholder="Enter a link" bind:value={link} />
+    <input type="text" placeholder="Enter custom text" bind:value={customText} />
     <button on:click={generateQrCode}>Generate QR Code</button>
-    {#if $qrCodeData}
-        <img src="{$qrCodeData}" alt="QR Code" />
+    {#if link}
+        <canvas bind:this={canvas}></canvas>
+        <a href="#" bind:this={downloadLink}>Download QR Code</a>
     {/if}
 </section>
 
@@ -29,7 +31,7 @@
         margin: 1em;
     }
 
-    img {
+    canvas {
         margin-top: 1em;
         width: 10em;
     }
@@ -45,22 +47,33 @@
     }
 </style>
 
-<script>
-    import { writable } from 'svelte/store';
+<script lang="ts">
+    import { onMount } from 'svelte';
     import QRCode from 'qrcode';
 
     let link = '';
-    let qrCodeData = writable('');
+    let customText = '';
+    let canvas;
+    let downloadLink;
 
     const generateQrCode = async () => {
         try {
             const options = {
                 errorCorrectionLevel: 'H',
-                width: 1024,
+                width: 512,
                 margin: 2
             };
-            const qrCode = await QRCode.toDataURL(link, options);
-            qrCodeData.set(qrCode);
+            await QRCode.toCanvas(canvas, link, options);
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+                ctx.font = '27px Arial';
+                const textWidth = ctx.measureText(customText).width;
+                const posX = 7;
+                const posY = canvas.height - 7;
+                ctx.fillText(customText, posX, posY);
+            }
+            downloadLink.href = canvas.toDataURL();
+            downloadLink.download = 'qrcode.png';
         } catch (error) {
             console.error('Error generating QR code', error);
         }
